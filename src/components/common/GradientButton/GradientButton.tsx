ca0fe299328d043b59efbfaf3f11833995e96dcd
vlikaa <aliveliyev123654@gmail.com>
2025-10-22 12:01:15 +0400

    Created GradientButton.tsx

---
+++ src/components/common/GradientButton/GradientButton.tsx
@@ -0,0 +1,11 @@
+function GradientButton() {
+    return (
+        <>
+            <button className="bg-[#FF6600] text-white px-[24px] py-[12px] rounded-[10px]">
+                Qoşulun
+            </button>
+        </>
+    )
+}
+
+export default GradientButton
