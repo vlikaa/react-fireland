6196de6bf7dc811fd9e1009bb8616590e0b3d836
vlikaa <aliveliyev123654@gmail.com>
2025-10-22 15:54:45 +0400

    Created a SectionTitle component

---
+++ src/components/common/SectionTitle/SectionTitle.tsx
@@ -0,0 +1,15 @@
+import type { ReactNode } from 'react';
+
+interface SectionTitleProps {
+	children: ReactNode
+}
+
+function SectionTitle({ children }: SectionTitleProps) {
+	return (
+		<h2 className="font-[Poppins] font-bold text-[32px] xl:text-[48px] bg-gradient-to-r from-[#FF6A00] to-[#FFB400] bg-clip-text text-transparent">
+			{ children }
+		</h2>
+	)
+}
+
+export default SectionTitle
