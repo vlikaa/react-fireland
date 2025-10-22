50d9c98cecebbbf0af1e0ddbfa17f23001281a92
vlikaa <aliveliyev123654@gmail.com>
2025-10-22 22:15:12 +0400

    Changed div tag to footer tag, renamed Button, added an info section

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,15 +1,19 @@
 import SectionTitle from '@/components/common/SectionTitle/SectionTitle.tsx';
-import GradientButton from '@/components/common/GradientButton/GradientButton.tsx';
+import Button from '@/components/common/Button/Button.tsx';
 
 function Footer() {
 	return (
-		<>
-			<div className="flex flex-col items-center ">
-				<SectionTitle>Qeydiyyatdan keç</SectionTitle>
-				<p className="mt-[30px] mb-[60px]">Öz komandana qoşul, ideyana güvən və FireLand-də gələcəyin bir parçası ol!</p>
-				<GradientButton></GradientButton>
+		<footer className="flex flex-col items-center  px-[20px] xl:px-[40px]">
+			<SectionTitle>Qeydiyyatdan keç</SectionTitle>
+			<p className="mt-[20px] mb-[50px] xl:mt-[30px] xl:mb-[60px] text-center">Öz komandana qoşul, ideyana güvən və FireLand-də gələcəyin
+				bir parçası ol!</p>
+			<Button background={"white"} size={'wide'}>Qoşulun</Button>
+			<div className="flex justify-center xl:justify-between w-full mt-[50px] xl:mt-[100px] mb-[50px]">
+				<p className="hidden xl:block">+994 50 500 50 50</p>
+				<p>© 2025 FireLand</p>
+				<p className="hidden xl:block">Example@gmail.com</p>
 			</div>
-		</>
+		</footer>
 	)
 }
 
