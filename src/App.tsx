bf639ca911c7415fd2ed03d9aef3c18998a257d0
vlikaa <aliveliyev123654@gmail.com>
2025-10-23 21:04:55 +0400

    Create a Hero section component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import './App.css'
 import Layout from '@/components/layout/Layout.tsx';
+import Hero from '@/components/sections/Hero/Hero.tsx';
+import AboutUs from '@/components/sections/AboutUs/AboutUs.tsx';
+import Information from '@/components/sections/Information/Information.tsx';
+import Prizes from '@/components/sections/Prizes/Prizes.tsx';
+import Sponsors from '@/components/sections/Sponsors/Sponsors.tsx';
 
 function App() {
     return (
         <>
             <Layout>
-				<h1>Hello World</h1>
+				<Hero />
+				<AboutUs />
+				<Information />
+				<Prizes />
+				<Sponsors />
 			</Layout>
         </>
     )
---
+++ src/components/sections/Hero/Hero.tsx
@@ -0,0 +1,25 @@
+import clock from '@assets/clock.svg'
+import pointer from '@assets/pointer.svg'
+import Button from '@/components/common/Button/Button.tsx';
+
+function Hero() {
+	return (
+		<div className="flex flex-col items-center bg-gradient-to-r from-[#FF6A00] to-[#FF9200] px-[20px] xl:px-[40px]">
+			<h1 className="font-poppins font-bold text-center text-[38px] xl:text-[64px] mt-[101px] xl:mt-[169px]">İdeyalarını alova çevir &lt;!&gt;</h1>
+			<p className="text-[16] xl:text-[20] font-inter font-medium text-center mt-[28px] xl:mt-[30px] mb-[68px] xl:mb-[77px]">Odlar Yurdu Universitetinin 30 illiyinə həsr olunmuş ilk böyük Hackathon</p>
+			<Button background={'white'} size={'wide'}>Qoşulun</Button>
+			<div className="w-full flex flex-col items-center gap-[20px] xl:flex-row xl:justify-between mt-[100px] xl:mt-[130px] mb-[50px] text-[16px] xl:text-[20px] font-rubik">
+				<div className="flex gap-[10px] xl:gap-[20px] items-center">
+					<img src={clock} alt="Clock" className="w-[20px] h-[20px] xl:w-[37.5px] xl:h-[37.5px]" />
+					<p>29 noyabr</p>
+				</div>
+				<div className="flex gap-[10px] xl:gap-[20px] items-center">
+					<img src={pointer} alt="Pointer" className="w-[20px] h-[20px] xl:w-[40px] xl:h-[40px]" />
+					<p>Odlar Yurdu universiteti</p>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Hero
