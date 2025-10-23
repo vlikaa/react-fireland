234a0538cd56ee38b375dc6181650ce992157e60
vlikaa <aliveliyev123654@gmail.com>
2025-10-23 10:35:17 +0400

    Fixed tailwind font argument

--- src/components/common/SectionTitle/SectionTitle.tsx
+++ src/components/common/SectionTitle/SectionTitle.tsx
@@ -6,7 +6,7 @@ interface SectionTitleProps {
 
 function SectionTitle({ children }: SectionTitleProps) {
 	return (
-		<h2 className="font-[Poppins] font-bold text-[32px] xl:text-[48px] bg-gradient-to-r from-[#FF6A00] to-[#FFB400] bg-clip-text text-transparent">
+		<h2 className="font-poppins font-bold text-[32px] xl:text-[48px] bg-gradient-to-r from-[#FF6A00] to-[#FFB400] bg-clip-text text-transparent">
 			{ children }
 		</h2>
 	)
