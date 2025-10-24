8baf88e4342eb31093eecaea202346c7bd10a3c2
vlikaa <aliveliyev123654@gmail.com>
2025-10-24 09:48:28 +0400

    Add onClick handler to Props and use it

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -4,9 +4,10 @@ interface GradientButtonProps {
 	children: ReactNode
 	background?: 'gradient' | 'white'
 	size?: 'wide' | 'narrow'
+	onClick?: () => void
 }
 
-function Button({ children, background = 'gradient', size = 'narrow'}: GradientButtonProps) {
+function Button({ children, background = 'gradient', size = 'narrow', onClick}: GradientButtonProps) {
 
 	const backgroundClasses = {
 		gradient: 'text-white bg-gradient-to-r from-[#FF6A00] to-[#FF0900] hover:from-[#FF6A00] hover:to-[#FFB400] active:from-[#FF0900] active:to-[#FF6A00]',
@@ -19,7 +20,7 @@ function Button({ children, background = 'gradient', size = 'narrow'}: GradientB
 	}
 
 	return (
-		<button className={`font-rubik h-[40px] w-[183px] rounded-[4px] text-[18px] xl:text-[20px] hover:cursor-pointer ${backgroundClasses[background]} ${sizeClasses[size]}`}>
+		<button className={`font-rubik h-[40px] w-[183px] rounded-[4px] text-[18px] xl:text-[20px] hover:cursor-pointer ${backgroundClasses[background]} ${sizeClasses[size]}`} onClick={onClick}>
 			{children}
 		</button>
 	)
