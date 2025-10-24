2f90c8f1721886f6c9f170f9e8d2c210960c45ed
vlikaa <aliveliyev123654@gmail.com>
2025-10-24 12:57:01 +0400

    Refactor spaces

--- src/components/common/Logo/Logo.tsx
+++ src/components/common/Logo/Logo.tsx
@@ -3,7 +3,7 @@ import logo from '@assets/fireland.svg'
 function Logo() {
     return (
         <div className="w-[100px] xl:w-[150px]">
-            <img src={logo} alt="Fireland"/>
+            <img src={logo} alt="Fireland" />
         </div>
     )
 }
