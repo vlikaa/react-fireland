5dda1c0cc5a0d4a9d1bb43a4dab4e15bc05adb2d
vlikaa <aliveliyev123654@gmail.com>
2025-10-24 10:04:17 +0400

    Add adaptive structure for mobile, add burger menu

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,17 +1,37 @@
-import Logo from '@/components/common/Logo/Logo';
-import NavigationBar from '@/components/layout/Header/NavigationBar/NavigationBar';
-import Button from '@/components/common/Button/Button.tsx';
-import LanguageSwitcher from '@/components/common/LanguageSwitcher/LanguageSwitcher';
+import { useState } from 'react'
+import Logo from '@/components/common/Logo/Logo'
+import NavigationBar from '@/components/layout/Header/NavigationBar/NavigationBar'
+import Button from '@/components/common/Button/Button.tsx'
+import LanguageSwitcher from '@/components/common/LanguageSwitcher/LanguageSwitcher'
+import BurgerButton from '@/components/common/BurgerButton/BurgerButton.tsx';
+import BurgerMenu from '@/components/common/BurgerMenu/BurgerMenu.tsx';
 
 function Header() {
+	const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+	const handleMenuToggle = () => {
+		setIsMenuOpen(!isMenuOpen)
+	}
+
+	const handleMenuClose = () => {
+		setIsMenuOpen(false)
+	}
+
 	return (
-		<header className="px-[24px] xl:px-[34px] h-[80px] bg-[#170A00] text-white flex items-center justify-between">
+		<header className="px-[24px] xl:px-[34px] h-[80px] bg-[#170A00] text-white flex items-center justify-between relative">
 			<Logo />
-			<NavigationBar />
-			<div className="flex items-center gap-[20px]">
+
+			<div className="hidden xl:block">
+				<NavigationBar />
+			</div>
+
+			<div className="hidden xl:flex xl:items-center xl:gap-[20px]">
 				<LanguageSwitcher />
 				<Button background={'gradient'}>Qoşulun</Button>
 			</div>
+
+			<BurgerButton isOpen={isMenuOpen} onClick={handleMenuToggle} />
+			<BurgerMenu isOpen={isMenuOpen} onClose={handleMenuClose} />
 		</header>
 	)
 }
