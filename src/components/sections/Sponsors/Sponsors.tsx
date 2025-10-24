235e7bdbaf7f6ea8290d0dd1bf930a57d106f4d6
vlikaa <aliveliyev123654@gmail.com>
2025-10-24 12:56:37 +0400

    Create a structure for Sponsors component

--- src/components/sections/Sponsors/Sponsors.tsx
+++ src/components/sections/Sponsors/Sponsors.tsx
@@ -1,7 +1,12 @@
+import iria from '@assets/iria.svg'
+import oyu from '@assets/oyu.svg'
+
+
 function Sponsors() {
 	return (
-		<div>
-			Sponsors Section
+		<div className="flex justify-center gap-[80px] xl:gap-[250px] py-[100px] xl:py-[150px]">
+			<img src={iria} alt="IRIA Logo" className="xl:h-[250px]" />
+			<img src={oyu} alt="OYU Logo" className="xl:h-[250px]" />
 		</div>
 	)
 }
