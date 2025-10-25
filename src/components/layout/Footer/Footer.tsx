8c989edbf2f57fc29c71a023c553f8f4a3ef4085
vlikaa <aliveliyev123654@gmail.com>
2025-10-25 18:44:16 +0400

    Refactor margin-top value

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -8,7 +8,7 @@ function Footer() {
 			<p className="mt-[20px] mb-[50px] xl:mt-[30px] xl:mb-[60px] text-center text-[16px] xl:text-[20px] font-rubik text-white">Öz komandana qoşul, ideyana güvən və FireLand-də gələcəyin
 				bir parçası ol!</p>
 			<Button background={"white"} size={'wide'}>Qoşulun</Button>
-			<div className="flex justify-center xl:justify-between w-full mt-[50px] xl:mt-[100px] mb-[30px]">
+			<div className="flex justify-center xl:justify-between w-full mt-[40px] xl:mt-[100px] mb-[30px]">
 				<p className="hidden xl:block text-[16px] xl:text-[20px] font-rubik  font-light text-white">+994 50 500 50 50</p>
 				<p className="text-[12px] xl:text-[20px] font-rubik  font-light text-white">© 2025 FireLand</p>
 				<p className="hidden xl:block text-[16px] xl:text-[20px] font-rubik  font-light text-white">Example@gmail.com</p>
